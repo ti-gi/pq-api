f6ba38c0fad185a0b1e2b056587c2e1360a6766e
ti-gi <djapic.t@gmail.com>
2021-09-19 19:58:47 +0200

    contestants

---
+++ pq-api/Controllers/ContestantsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pq_api.data.Entities;
+using M = pq_api.Models;
+using B = pq_api.service.BusinessModels;
+using pq_api.service;
+
+namespace pqWebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ContestantsController : ControllerBase
+    {
+        //private readonly pqsightcom_dev_core_1Context _context;
+
+        private IAppService appService;
+
+        public ContestantsController(IAppService appService)
+        {
+            this.appService = appService;
+        }
+
+        [Authorize]
+        [HttpGet("contestants/{ContestantId}")]
+        public M.Contestant GetContestant(int ContestantId)
+        {
+
+            var contestant = appService.GetContestant(ContestantId);
+            return new M.Contestant
+            {
+                id = contestant.Id,
+                competitionId = contestant.CompetitionId,
+                name = contestant.Name
+
+            };
+        }
+
+        [Authorize]
+        [HttpPost("contestants/add")]
+        public M.Contestant CreateContestant(M.ContestantCreate c)
+        {
+
+            var addedContestant = appService.AddContestant(new B.Contestant { Name = c.Name, CompetitionId = c.CompetitionId });
+            return new M.Contestant
+            {
+                id = addedContestant.Id,
+                name = addedContestant.Name
+
+            };
+
+        }
+
+        [Authorize]
+        [HttpPost("contestants/update")]
+        public M.Contestant UpdateContestant(M.ContestantUpdate c)
+        {
+
+            var updatedContestant = appService.EditContestant(new B.Contestant { Id = c.Id, Name = c.Name, CompetitionId = c.CompetitionId });
+            return new M.Contestant
+            {
+                id = updatedContestant.Id,
+                name = updatedContestant.Name
+
+            };
+        }
+
+
+        }
+    }
---
+++ pq-api/Models/ContestantCreate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pq_api.Models
+{
+    public class ContestantCreate
+    {
+        public int CompetitionId { get; set; }
+        public string Name { get; set; }
+    }
+}
